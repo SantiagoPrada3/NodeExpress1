e21081f0b265f34ab86d5338ae4f796b2d97dec2
Ubuntu <ubuntu@ip-172-31-43-178.ec2.internal>
2023-11-05 00:15:20 +0000

    archivos update

---
+++ LandingPage/index.js
@@ -0,0 +1,12 @@
+const http= require('http');
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname+'/'));
+
+app.get('/',(req,res) => {
+    res.sendFile("/home/ubuntu/LandinPage/index.html")
+});
+
+app.listen(3001);
+console.log('Server on port 3001');
